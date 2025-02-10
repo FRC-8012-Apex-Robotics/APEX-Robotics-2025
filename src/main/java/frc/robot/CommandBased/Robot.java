36a053321da943c7fc7680e908a2a558ce5be6f8
DynamiteChains1 <jfp6724@students.mpsaz.org>
2025-02-10 16:11:00 -0700

    command-based now fully functional (maybe)

--- src/main/java/frc/robot/CommandBased/Robot.java
+++ src/main/java/frc/robot/CommandBased/Robot.java
@@ -1,7 +1,18 @@
 package frc.robot.CommandBased;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 public class Robot extends TimedRobot {
-    
+    private final RobotContainer m_container = new RobotContainer();
+
+    public Robot(){
+        m_container.configureBindings();
+    }
+
+    @Override
+    public void robotPeriodic(){
+
+        CommandScheduler.getInstance().run();
+    }
 }
