2d896ab96b26c88b25b0a3b060016ba76567fbc0
DynamiteChains1 <jfp6724@students.mpsaz.org>
2025-02-10 17:21:24 -0700

    added intake command subsystem

--- src/main/java/frc/robot/CommandBased/RobotContainer.java
+++ src/main/java/frc/robot/CommandBased/RobotContainer.java
@@ -2,9 +2,11 @@ package frc.robot.CommandBased;
 
 import edu.wpi.first.wpilibj2.command.button.CommandGenericHID;
 import frc.robot.CommandBased.subsystems.Drive;
+import frc.robot.CommandBased.subsystems.Intake;
 
 public class RobotContainer {
     private final Drive m_drive = new Drive();
+    private final Intake m_intake = new Intake();
 
     private final CommandGenericHID m_generic = new CommandGenericHID(0);
 
@@ -13,6 +15,8 @@ public class RobotContainer {
             m_drive.arcadeDriveCommand(
             () -> m_generic.getRawAxis(0), () -> Math.pow(m_generic.getRawAxis(1), 2))
             );
+
+        m_generic.button(1).onTrue(m_intake.armDown());
     }
     
 }
--- src/main/java/frc/robot/CommandBased/subsystems/Intake.java
+++ src/main/java/frc/robot/CommandBased/subsystems/Intake.java
@@ -1,11 +1,27 @@
 package frc.robot.CommandBased.subsystems;
 
-import rev
+import java.util.function.DoubleSupplier;
 
-public class Intake {
-    Rev
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Intake extends SubsystemBase {
+    private final SparkMax m_arm = new SparkMax(6, MotorType.kBrushless);
 
     public Intake(){
+        m_arm.getEncoder().setPosition(0);
+        m_arm.set(0.5);
+    }
 
+    public Command armDown(){
+        return runOnce(() -> {
+            while (m_arm.getEncoder().getPosition() < 0.6){
+                m_arm.set(0.4);
+            }
+            m_arm.stopMotor();
+        });
     }
 }
