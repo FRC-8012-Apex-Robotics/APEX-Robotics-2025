a204109727850bb024eea3a854b47f2064978192
DynamiteChains1 <141443640+DynamiteChains1@users.noreply.github.com>
2025-02-13 09:57:47 -0700

    Update Intake.java
    Added method for controlling the speed the arm rotates at with SmartDashboard

--- src/main/java/frc/robot/CommandBased/subsystems/Intake.java
+++ src/main/java/frc/robot/CommandBased/subsystems/Intake.java
@@ -12,10 +12,12 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Intake extends SubsystemBase {
     private final SparkMax m_arm = new SparkMax(4, MotorType.kBrushless);
+    private final SparkMax m_intake = new SparkMax(5, MotorType.kBrushless);
     private final Timer intakeTimer = new Timer();
 
     public Intake(){
         m_arm.getEncoder().setPosition(0);
+        SmartDashboard.putDouble("Arm Speed", 0.4);
 
         setDefaultCommand(
             runOnce(() -> {
@@ -28,7 +30,7 @@ public class Intake extends SubsystemBase {
     public Command armDown(){
         return runOnce(() -> {
             while (m_arm.getEncoder().getPosition() < 0.6){
-                m_arm.set(0.4);
+                m_arm.set(SmartDashboard.getDouble("Arm Speed"));
             }
             m_arm.stopMotor();
         
@@ -39,15 +41,11 @@ public class Intake extends SubsystemBase {
     public Command armUp(){
         return runOnce(() -> {
             while (m_arm.getEncoder().getPosition() > 0.1){
-                m_arm.set(-0.4);
+                m_arm.set(SmartDashboard.getDouble("Arm Speed"));
             }
             m_arm.stopMotor();
 
             SmartDashboard.putString("Arm Position", "up");
         });
-    }
-
-    public void armControl(double speed) {
-        m_arm.set(speed);
     }
 }
