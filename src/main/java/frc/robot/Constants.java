73490b5a74ca36fc6f5400a3525884b3b3696669
DynamiteChains1 <jfp6724@students.mpsaz.org>
2025-03-13 21:40:41 -0700

    Doubled the speed at which the robot ejects coral during autonomous

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -15,7 +15,7 @@ public class Constants {
         public static final int ARM_ID = 4;
         public static final int SPIN_ID = 5;
         public static final double INTAKE_SPEED = 1;
-        public static final double CORAL_EJECT_SPEED = 0.5;
+        public static final double CORAL_EJECT_SPEED = 1;
         public static final double ARM_P = 2;
         public static final double ARM_I = 0;
         public static final double ARM_D = 0.001;
